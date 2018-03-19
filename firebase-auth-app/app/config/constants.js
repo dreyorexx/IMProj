625800ceea9010922113527408a49332907d8f37
dreyorexx <coolfireballs@gmail.com>
2018-03-19 11:15:33 +0800

    Added Redux

---
+++ firebase-auth-app/app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyAxqL0ILLVVrjiKBcaW0pye78-wO98iO2Y";
+export const FIREBASE_AUTH_DOMAIN = "improj-2c861.firebaseapp.com"
+export const FIREBASE_DATABASE_URL = "https://improj-2c861.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "improj-2c861";
+export const FIREBASE_STORAGE_BUCKET = "improj-2c861.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "1059746073255";
+
+export const FACEBOOK_APP_ID = "978440822303796";
